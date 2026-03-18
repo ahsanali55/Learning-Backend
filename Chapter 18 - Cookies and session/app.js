8858d8e3fe71a03a5059f14d2d1e2ee08df13365
ahsanali555 <ahsanalijawad599@gmail.com>
2026-03-19 02:26:27 +0500

    Chapter -18 cookies and session copy

---
+++ Chapter 18 - Cookies and session/app.js
@@ -0,0 +1,38 @@
+// Core Module
+const path = require("path");
+
+// External Module
+const express = require("express");
+
+//Local Module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded());
+app.use(storeRouter);
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(errorsController.pageNotFound);
+
+const PORT = 3000;
+
+const DB_PATH =  "mongodb://ahsanalijawad599:ahsan599@ac-kfoohql-shard-00-00.sh1jstp.mongodb.net:27017,ac-kfoohql-shard-00-01.sh1jstp.mongodb.net:27017,ac-kfoohql-shard-00-02.sh1jstp.mongodb.net:27017/airbnb?ssl=true&replicaSet=atlas-el19js-shard-0&authSource=admin&appName=Ahsan-Ali"
+
+mongoose.connect(DB_PATH).then(() => {
+  console.log("Connected to Mongodb");
+  app.listen(PORT, () => {
+    console.log(`Server running on address http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.log("Error while connecting to mongodb ", err);
+})
