a96f1a8f41575595e2f89fe74caa2d52cbff7332
ahsanali555 <ahsanalijawad599@gmail.com>
2026-03-29 00:18:41 +0500

    validation is going to learning

--- Chapter 19 - Authentication and Authorization/controllers/authController.js
+++ Chapter 19 - Authentication and Authorization/controllers/authController.js
@@ -1,3 +1,4 @@
+const { check, validationResult } = require("express-validator");
 exports.getLogin = (req, res, next) => {
   console.log("Get Login page request ");
   res.render("auth/login", {
@@ -21,12 +22,88 @@ exports.postLogout = (req, res, next) => {
 exports.getSignUp = (req, res, next) => {
   res.render("auth/signup", {
     pageTitle: "SignUp",
-    currentPage: "signup",
+    currentPage: "signUp",
     isLoggedIn: false,
+    errors: [],
+    oldInput: { firstName: "", lastName: "", password: "", userType: "" },
   });
 };
 
-exports.postSignUp = (req, res, next) => {
-  console.log("Post signup request getting ", req.body);
-  res.redirect('/login')
-};
+exports.postSignUp = [
+  // First Name validation
+  check("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First name must be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First name should only alphabets letter"),
+
+  // Last Name validation
+  check("lastName")
+    .notEmpty()
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("Last name should only alphabets letter"),
+
+  // Email validation
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  // Passward validation
+  check("password")
+    .isLength({ min: 5 })
+    .withMessage("Passward should atleast 8 characters long")
+    .matches(/^[A-Z]/)
+    .withMessage("Passward should contain atleast one uppercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Passward should contain atleast one number")
+    .matches(/[!@#$%*&^]/)
+    .withMessage("Pasward should contain atleast one special characters")
+    .trim(),
+
+  // Confirm passward validation
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password do not matches");
+      }
+      return true;
+    }),
+
+  // userType validation
+  check("userType")
+    .notEmpty()
+    .isIn("guest", "host")
+    .withMessage("Invalid user type"),
+
+  // Terms validation
+  check("terms")
+    .notEmpty()
+    .withMessage("Please accept the terms and conditions")
+    .custom((value, { req }) => {
+      if (value !== "on") {
+        throw new Error("Please accept the terms and conditions");
+      }
+      return true;
+    }),
+  (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    console.log("The errors ", errors)
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Sign Up",
+         currentPage: "signUp",
+        isLoggedIn: false,
+        errors: errors.array().map((error) => error.msg),
+        oldInput: { firstName, lastName, password, userType },
+      });
+    }
+    console.log("Post signup request getting ", req.body);
+    res.redirect("/login");
+  },
+];
